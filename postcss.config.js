d2bb86af4e68738c87c2d8d59d10867171c5fffa
aman-anand17 <legendneverretires@gmail.com>
2025-10-05 01:23:14 +0530

    portfolio code

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const plugin = require("@tailwindcss/forms");
+const tailwindConfig = require("./tailwind.config");
+
+module.exports ={
+    plugin:{
+        tailwindcss:{},
+        autoprefixer: {}
+    }
+}
